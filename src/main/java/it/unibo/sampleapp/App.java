efbb747fa316345b61179af6ed2a7518dda9a838
Niccolò Maltoni <nicco.mlt@gmail.com>
2021-02-26 12:04:54 +0100

    style: fix QA plugins warnings (#1)

--- src/main/java/it/unibo/sampleapp/App.java
+++ src/main/java/it/unibo/sampleapp/App.java
@@ -17,16 +17,21 @@ import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-public class App {
+public final class App {
     private static final String DEFAULT_AUTHOR = "Camilleri";
     private static final String GOOGLE_BOOKS_API_KEY = System.getenv("GOOGLE_BOOKS_API_KEY");
     private static final String OMDB_API_KEY = System.getenv("OMDB_API_KEY");
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-    public static void main(String[] args) throws IOException, GeneralSecurityException {
+    private App() {
+        // the constructor will never be called directly.
+    }
+
+    public static void main(final String[] args) throws IOException, GeneralSecurityException {
         final OmdbApi omdb = new OmdbApi(OMDB_API_KEY);
         final String author = Arrays.stream(args).findFirst().orElse(DEFAULT_AUTHOR);
 
@@ -37,11 +42,22 @@ public class App {
             // 0. Here we get a Books object (API façade)
             .volumes().list("inauthor: " + author)
             .execute().getItems().parallelStream()
-            .filter(v -> v.getVolumeInfo().getAuthors().stream().anyMatch(a -> a.toLowerCase().contains(author.toLowerCase())))
+            .filter(v -> v
+                    .getVolumeInfo()
+                    .getAuthors()
+                    .stream()
+                    .anyMatch(a -> a
+                            .toLowerCase(Locale.getDefault())
+                            .contains(author.toLowerCase(Locale.getDefault()))))
             .map(v -> v.getVolumeInfo().getTitle())
             // 1. Here we get a stream of book titles
-            .flatMap(Errors.rethrow().<String,Stream<Pair<String,String>>>wrap(title -> Optional.ofNullable(
-                    omdb.search(new OmdbBuilder().setSearchTerm(URLEncoder.encode(title, StandardCharsets.UTF_8)).build()).getResults())
+            .flatMap(Errors
+                    .rethrow()
+                    .<String, Stream<Pair<String, String>>>wrap(title -> Optional.ofNullable(omdb
+                            .search(new OmdbBuilder()
+                                    .setSearchTerm(URLEncoder.encode(title, StandardCharsets.UTF_8))
+                                    .build())
+                            .getResults())
                     .map(r -> r.stream().map(m -> Pair.of(m.getTitle(), m.getYear()))
             ).orElseGet(Stream::empty)))
             // 2. Here we get a stream of movies as pairs (title, year)
