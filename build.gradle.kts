97a36b8a50d7aaca605437bd541f665d1f8bddd1
Danilo Pianini <danilo.pianini@unibo.it>
2022-04-19 13:08:47 +0200

    chore!: simplify the app and its dependencies, improve the build and CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,28 +20,28 @@ repositories {
 
 dependencies {
     // Maven dependencies are composed by a group name, a name and a version, separated by colons
-    implementation("com.diffplug.durian:durian:3.4.0")
-    implementation("com.google.apis:google-api-services-books:v1-rev20201021-1.31.0")
     implementation("com.omertron:API-OMDB:1.5")
+    implementation("org.jooq:jool:0.9.14")
 
     /*
      * Simple Logging Facade for Java (SLF4J) with Apache Log4j
      * See: http://www.slf4j.org/
      */
     val slf4jVersion = "1.7.36"
-    // when dependencies share the same version, grouping in a val helps to keep them in sync
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    runtimeOnly("org.slf4j:slf4j-log4j12:$slf4jVersion")
+    // Logback backend for SLF4J
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.11")
 
     // JUnit API and testing engine
     val jUnitVersion = "5.8.2"
+    // when dependencies share the same version, grouping in a val helps to keep them in sync
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
 }
 
 application {
     // Define the main class for the application.
-    mainClass.set("it.unibo.sampleapp.App")
+    mainClass.set("it.unibo.sampleapp.RateAMovie")
 }
 
 java {
--- src/test/java/it/unibo/sampleapp/AppTest.java
+++ src/test/java/it/unibo/sampleapp/AppTest.java
@@ -3,12 +3,19 @@
  */
 package it.unibo.sampleapp;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class AppTest {
     @Test public void testAppHasAGreeting() {
-        assertTrue(true);
+        assertNotNull(System.getenv("OMDB_API_KEY"));
+        assertFalse(System.getenv("OMDB_API_KEY").isBlank());
+        RateAMovie.main(new String[] { "Breaking Bad" });
     }
 }
