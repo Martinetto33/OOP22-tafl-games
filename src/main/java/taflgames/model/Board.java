c774e896529aceb0a5914cf155febbaa7916eddd
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-02-24 14:58:46 +0100

    chore: added temp empty class

---
+++ src/main/java/taflgames/model/Board.java
@@ -0,0 +1,5 @@
+package taflgames.model;
+
+public class Board {
+    
+}
--- src/main/java/taflgames/model/common/api/Vector.java
+++
@@ -1,7 +0,0 @@
-package taflgames.model.common.api;
-
-/**
- * This interface is fake for now and will model a mathematic vector.
- */
-public interface Vector {
-}
