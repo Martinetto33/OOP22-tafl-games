7e370245b7ebf104db6e18cd2e13ac94784f45fe
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-02-24 15:41:08 +0100

    chore: updated fake Board interface

--- src/main/java/taflgames/model/Board.java
+++ src/main/java/taflgames/model/Board.java
@@ -1,5 +1,19 @@
 package taflgames.model;
 
-public class Board {
+import taflgames.common.api.Position;
+import taflgames.common.api.Vector;
+import taflgames.common.Player;
+
+public interface Board {
     
+    boolean isStartingPointValid(Position start, Player player);
+
+    boolean isDestinationValid(Position start, Position dest, Player player);
+
+    void updatePiecePos(Position oldPos, Position newPos);
+
+    Position getFurthestReachablePos(Vector direction);
+
+    void moveByVector(Vector direction);
+
 }
