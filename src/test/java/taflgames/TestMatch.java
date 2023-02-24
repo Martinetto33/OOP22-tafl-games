2a31f03b4e95b589eca58e6d058f492a18d2b151
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-02-24 19:26:58 +0100

    test: added (empty) test class

---
+++ src/test/java/taflgames/TestMatch.java
@@ -0,0 +1,12 @@
+package taflgames;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import taflgames.model.Match;
+
+/**
+ * JUnit tests for {@link Match}.
+ */
+public class TestMatch {
+    
+}
