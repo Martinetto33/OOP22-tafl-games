7ca683d7311d36d327e4164ad5d9a0e817f2395e
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-02-24 17:52:39 +0100

    feat: added player turn queue implementation

--- src/main/java/taflgames/model/MatchImpl.java
+++ src/main/java/taflgames/model/MatchImpl.java
@@ -1,33 +1,58 @@
 package taflgames.model;
 
+import java.util.List;
+
+import org.apache.commons.collections4.iterators.LoopingIterator;
+
 import taflgames.common.Player;
 import taflgames.common.api.Position;
 
 public final class MatchImpl implements Match {
     
     private final Board board = null;   // TO INITIALIZE
+    /*
+     * Using a collection from the Apache Commons Collection library to implement
+     * the players turn queue
+     */
+    private final LoopingIterator<Player> turnQueue;
     private Player playerInTurn;
 
     public MatchImpl() {
+        this.turnQueue = new LoopingIterator<>(
+            List.of(Player.ATTACKER, Player.DEFENDER)
+        );
+        this.setNextPlayerInTurn();
+    }
 
+    @Override
+    public Player getNextPlayernTurn() {
+        return this.playerInTurn;
+    }
+
+    @Override
+    public void setNextPlayerInTurn() {
+        this.playerInTurn = this.turnQueue.next();
     }
 
     @Override
     public boolean selectSource(final Position start) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'selectSource'");
+        return this.board.isStartingPointValid(start, playerInTurn);
     }
 
     @Override
     public boolean selectDestination(final Position start, final Position destination) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'selectDestination'");
+        return this.board.isDestinationValid(start, destination, playerInTurn);
     }
 
     @Override
     public void makeMove(final Position start, final Position destination) {
+        this.board.updatePiecePos(start, destination);
+    }
+
+    @Override
+    public boolean isOver() {
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'makeMove'");
+        throw new UnsupportedOperationException("Unimplemented method 'isOver'");
     }
 
 }
