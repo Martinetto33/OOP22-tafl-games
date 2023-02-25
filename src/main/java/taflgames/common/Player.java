867f6e9b4be5c34156d3f4b93f68b7250e8230d6
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-02-25 10:51:40 +0100

    style: add javadoc comments

--- src/main/java/taflgames/common/Player.java
+++ src/main/java/taflgames/common/Player.java
@@ -4,6 +4,12 @@ package taflgames.common;
  * Represents the role of the player during the match.
  */
 public enum Player {
+    /**
+     * The player that owns the attacking team.
+     */
     ATTACKER,
+    /**
+     * The player that owns the defending team.
+     */
     DEFENDER
 }
