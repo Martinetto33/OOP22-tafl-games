44ec70104d4523db68bbaa91464e17e51fbd41fa
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-02-25 10:52:43 +0100

    style: delete trailing spaces

--- src/main/java/taflgames/model/Board.java
+++ src/main/java/taflgames/model/Board.java
@@ -5,7 +5,7 @@ import taflgames.common.api.Vector;
 import taflgames.common.Player;
 
 public interface Board {
-    
+
     boolean isStartingPointValid(Position start, Player player);
 
     boolean isDestinationValid(Position start, Position dest, Player player);
