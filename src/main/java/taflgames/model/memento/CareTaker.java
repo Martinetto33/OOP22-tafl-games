1c51c5c8377bd30f78d37432d1b810ac684304e3
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2023-02-28 19:23:09 +0100

    feat: add memento interfaces for all major classes

--- src/main/java/taflgames/model/memento/CareTaker.java
+++ src/main/java/taflgames/model/memento/CareTaker.java
@@ -1,27 +1,44 @@
 package taflgames.model.memento;
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 
-public class CareTaker {
-    private MatchExample originator;
-    private List<MatchExample.MatchMemento> history;
+/**
+ * This class will model a Caretaker, a class which is part of the pattern Memento
+ * and that will be in charge of managing the history of a single Match, which here
+ * takes the name of "originator".
+ * <br>The updateHistory() method should be called each time a turn ends, while
+ * the undo() method could be called when the user presses an "undo" button.
+ * 
+ * 
+ * This class will be implemented with a Stack, in order to allow methods 
+ * "pop" and "push" on the history
+ */
+public class Caretaker {
+    //private MatchExample originator;
+    //private List<MatchMemento> history;
 
-    public CareTaker(MatchExample originator) {
+    /**
+     * Builds a new Caretaker.
+     */
+    /* public Caretaker(MatchExample originator) {
         this.originator = originator;
         this.history = new ArrayList<>();
+    } */
+
+    /**
+     * Registers a new MatchMemento, by pushing it onto the history stack.
+     */
+    public void updateHistory() {
+        //this.history.add(this.originator.save());
     }
 
-    public void doStuff() {
-        this.history.add(this.originator.save());
-    }
-
-    public void undo(int turnNumber) {
+    /* public void undo(int turnNumber) throws IllegalAccessError {
         if(turnNumber >= 0 && turnNumber < this.history.size()) {
             this.originator.restore(this.history.get(turnNumber));
         }
         else {
-            System.out.println("Error occurred");
+            throw new IllegalAccessError();
         }
-    }
+    } */
 }
---
+++ src/main/java/taflgames/model/memento/MatchMemento.java
@@ -0,0 +1,18 @@
+package taflgames.model.memento;
+
+/**
+ * An interface modelling the behaviour that the Inner Class of the Match implementation
+ * should provide in order to save a snapshot of the current state of a Match. Said Inner
+ * Class should implement this interface.
+ * <br>This interface is part of the pattern Memento.
+ */
+public interface MatchMemento {
+    /**
+     * Since the Match has a pointer to the Board, after restoring its state from
+     * this Match Memento the "restore()" method of the class Match should call
+     * "board.save()" by passing this BoardMemento as a parameter.
+     * @return a BoardMemento from which the board can restore its previous state.
+     */
+    BoardMemento getBoardMemento();
+
+}
