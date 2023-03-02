883d57e740f843b964164a64cf5e5bd209719f04
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-03-02 18:30:22 +0100

    feat: add classic mode settings loading

--- src/main/java/taflgames/controller/SettingsLoader.java
+++ src/main/java/taflgames/controller/SettingsLoader.java
@@ -1,5 +1,13 @@
 package taflgames.controller;
 
+import java.io.IOException;
+
+import taflgames.model.BoardBuilder;
+
 public interface SettingsLoader {
+
+    void loadClassicModeConfig(final BoardBuilder boardBuilder) throws IOException;
+
+    void loadVariantModeConfig(final BoardBuilder boardBuilder) throws IOException;
 
 }
