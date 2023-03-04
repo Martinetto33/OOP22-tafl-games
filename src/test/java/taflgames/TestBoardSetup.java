728f0d83cf3283d96ed611698c53b0486619eeb4
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-03-04 11:51:31 +0100

    test: add completed test for board setup in classic mode

--- src/test/java/taflgames/TestBoardSetup.java
+++ src/test/java/taflgames/TestBoardSetup.java
@@ -1,10 +1,11 @@
 package taflgames;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,9 @@ import taflgames.controller.SettingsLoader;
 import taflgames.controller.SettingsLoaderImpl;
 import taflgames.model.BoardBuilder;
 import taflgames.model.cells.Cell;
+import taflgames.model.cells.Exit;
+import taflgames.model.cells.Throne;
+import taflgames.model.pieces.BasicPiece;
 import taflgames.model.pieces.King;
 import taflgames.model.pieces.Piece;
 import taflgames.common.Player;
@@ -23,7 +27,7 @@ import taflgames.common.Player;
  * that receives the configuration data from a {@link SettingsLoader}.
  */
 class TestBoardSetup {
-    
+
     private BoardBuilder builder;
 
     @BeforeEach
@@ -35,19 +39,98 @@ class TestBoardSetup {
      * Test the board building according to the classic mode settings.
      */
     @Test
-    void TestClassicBoardConfigFromFile() {
+    void TestClassicBoardConfig() {
         final SettingsLoader loader = new SettingsLoaderImpl();
         try {
-            final BoardBuilder builder = new BoardBuilder();
+            // Load classic mode game settings
             loader.loadClassicModeConfig(this.builder);
+            
+            // Retrieve the collections of cells and pieces built according to the settings
             final Map<Position, Cell> cells = this.builder.getCells();
             final Map<Player, Map<Position, Piece>> pieces = this.builder.getPieces();
+
+            // Check king and throne correct placement
             assertEquals(
                 pieces.get(Player.DEFENDER).get(new Position(5, 5)).getClass(),
                 King.class
             );
+            assertEquals(
+                cells.get(new Position(5, 5)).getClass(),
+                Throne.class
+            );
+
+            // Check exits correct placement
+            assertEquals(
+                getPositions(cells, Exit.class),
+                Set.of(
+                    new Position(0, 0),
+                    new Position(0, 10),
+                    new Position(10, 0),
+                    new Position(10, 10)
+                )
+            );
+
+            // Check attacker basic pieces correct placement
+            assertEquals(
+                getPositions(pieces.get(Player.ATTACKER), BasicPiece.class),
+                Set.of(
+                    new Position(0, 3),
+                    new Position(0, 4),
+                    new Position(0, 5),
+                    new Position(0, 6),
+                    new Position(0, 7),
+                    new Position(1, 5),
+                    new Position(3, 0),
+                    new Position(4, 0),
+                    new Position(5, 0),
+                    new Position(6, 0),
+                    new Position(7, 0),
+                    new Position(5, 1),
+                    new Position(5, 9),
+                    new Position(3, 10),
+                    new Position(4, 10),
+                    new Position(5, 10),
+                    new Position(6, 10),
+                    new Position(7, 10),
+                    new Position(9, 5),
+                    new Position(10, 3),
+                    new Position(10, 4),
+                    new Position(10, 5),
+                    new Position(10, 6),
+                    new Position(10, 7)
+                )
+            );
+
+            // Check defender basic pieces correct placement
+            assertEquals(
+                getPositions(pieces.get(Player.DEFENDER), BasicPiece.class),
+                Set.of(
+                    new Position(3, 5),
+                    new Position(4, 4),
+                    new Position(4, 5),
+                    new Position(4, 6),
+                    new Position(5, 3),
+                    new Position(5, 4),
+                    new Position(5, 6),
+                    new Position(5, 7),
+                    new Position(6, 4),
+                    new Position(6, 5),
+                    new Position(6, 6),
+                    new Position(7, 5)
+                )
+            );
+            
         } catch (final IOException e) {
             e.printStackTrace();
         }
     }
+
+    private <C> Set<Position> getPositions(final Map<Position, C> map, final Class<? extends C> targetClass) {
+        return map.entrySet().stream()
+                // filter out the objects that are not instances of the target class
+                .filter(entry -> (entry.getValue().getClass() == targetClass))
+                .map(entry -> entry.getKey())   // get the positions of objects that are instances of the target class
+                .collect(Collectors.toSet());
+    }
+
 }
