626364a419fa82ffb18fab68d692dede533a6198
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-03-10 14:26:08 +0100

    feat: add basic implementation of the user registration scene

---
+++ src/main/java/taflgames/view/scenes/UserRegistrationScene.java
@@ -0,0 +1,39 @@
+package taflgames.view.scenes;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import taflgames.view.View;
+
+/**
+ * Implementation of the user registration scene.
+ */
+public class UserRegistrationScene extends AbstractScene {
+
+    private static final String USER_REGISTRATION = "User Registration";
+    private static final String GO_BACK = "Go Back";
+
+    /**
+     * Creates the user registration scene.
+     * @param view the view that displays the scene
+     */
+    public UserRegistrationScene(final View view) {
+
+        super(USER_REGISTRATION);
+
+        final JPanel scenePanel = super.getScene();
+        scenePanel.setLayout(new BoxLayout(scenePanel, BoxLayout.Y_AXIS));
+
+        final JPanel southPanel = new JPanel();
+        final JButton goBackButton = new JButton(GO_BACK);
+        southPanel.add(goBackButton);
+
+        goBackButton.addActionListener((e) -> {
+            view.setScene(new GameChoiceScene(view));
+        });
+
+        scenePanel.add(southPanel);
+    }
+
+}
