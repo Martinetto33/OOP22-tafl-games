92e32ad1db9eba53bb7ba4a0c3ab057981db2971
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2023-03-12 22:06:10 +0100

    docs: created javadoc for FontManager

--- src/main/java/taflgames/view/fontManager/FontManager.java
+++ src/main/java/taflgames/view/fontManager/FontManager.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 
+/**
+ * This class can use a custom font located in the resources packages.
+ * Any .ttf file can be potentially used. A rune-like font was chosen to fit
+ * better the style of the game.
+ */
 public class FontManager {
 
     private static final String SEP = System.getProperty("file.separator");
@@ -13,6 +18,9 @@ public class FontManager {
     private static final String FONT_FILE_NAME = "latin_runes_v20.ttf";
     private Font font;
 
+    /**
+     * Builds a new FontManager, initialising its font.
+     */
     public FontManager(){
         this.font = this.initialise(FontManager.FONT_FILE_NAME);
     }
@@ -33,10 +41,21 @@ public class FontManager {
         return null;
     }
 
+    /**
+     * A 1-sized instance of the font this class manages.
+     * @return the Font
+     */
     public Font getFont() {
         return this.font;
     }
 
+    /**
+     * A custom-sized and styled font.
+     * @param size the dimension of the font (normal JButtons use font size 12)
+     * @param type the style of the font, which should be a constant defined in the class Font
+     * (e.g. Font.PLAIN)
+     * @return the modified Font
+     */
     public Font getModifiedFont(float size, int type) {
         Font result = this.font.deriveFont(type, size);
         return result;
