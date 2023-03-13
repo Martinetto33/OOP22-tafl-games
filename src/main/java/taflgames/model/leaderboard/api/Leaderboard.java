8d1621f5770c552764d4f5b88fa5cd026c4782e2
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2023-03-13 22:43:28 +0100

    feat: created class and interface for the leaderboard

---
+++ src/main/java/taflgames/model/leaderboard/api/Leaderboard.java
@@ -0,0 +1,10 @@
+package taflgames.model.leaderboard.api;
+
+/**
+ * The Leaderboard will store all the results of the matches. At the end of
+ * each match, the players will be asked to insert their names, and their
+ * results will be saved.
+ */
+public interface Leaderboard {
+    
+}
---
+++ src/main/java/taflgames/model/leaderboard/code/LeaderBoardImpl.java
@@ -0,0 +1,7 @@
+package taflgames.model.leaderboard.code;
+
+import taflgames.model.leaderboard.api.Leaderboard;
+
+public class LeaderBoardImpl implements Leaderboard {
+    
+}
