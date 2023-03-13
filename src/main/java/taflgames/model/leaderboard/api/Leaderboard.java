af1bca795a950a24aff38cd18487111ec788815f
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2023-03-13 23:42:11 +0100

    feat: added methods for getting an ordered leaderboard, adding results and searching a player's results

--- src/main/java/taflgames/model/leaderboard/api/Leaderboard.java
+++ src/main/java/taflgames/model/leaderboard/api/Leaderboard.java
@@ -1,5 +1,11 @@
 package taflgames.model.leaderboard.api;
 
+import java.util.Map;
+import java.util.Optional;
+
+import taflgames.common.code.MatchResult;
+import taflgames.common.code.Pair;
+
 /**
  * The Leaderboard will store all the results of the matches. At the end of
  * each match, the players will be asked to insert their names, and their
@@ -7,4 +13,26 @@ package taflgames.model.leaderboard.api;
  */
 public interface Leaderboard {
     
+    /**
+     * Adds a new result at the end of a Match.
+     * @param playerName the name of one of the two players
+     * @param result the result obtained by the specified player
+     */
+    void addResult(String playerName, MatchResult result);
+
+    /**
+     * Returns the Leaderboard represented by a Map which has player names as keys
+     * and Pairs of number of wins and number of losses as values.
+     * @return a Map representing the current state of the Leaderboard, sorted by
+     * number of wins of each player.
+     */
+    Map<String, Pair<Integer, Integer>> getLeaderboard();
+
+    /**
+     * Gets the results associated with the specified player name, if there are any.
+     * @param playerName the name of the player whose results are to be searched
+     * @return an Optional containing the result represented by a Pair of wins and losses,
+     * or an empty Optional if the player hasn't registered any results yet.
+     */
+    Optional<Pair<Integer, Integer>> getScoreFromPlayer(String playerName);
 }
--- src/main/java/taflgames/model/leaderboard/code/LeaderBoardImpl.java
+++ src/main/java/taflgames/model/leaderboard/code/LeaderBoardImpl.java
@@ -1,7 +1,56 @@
 package taflgames.model.leaderboard.code;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import taflgames.common.code.MatchResult;
+import taflgames.common.code.Pair;
 import taflgames.model.leaderboard.api.Leaderboard;
 
 public class LeaderBoardImpl implements Leaderboard {
-    
+
+    private Map<String, Pair<Integer, Integer>> results;
+
+    public LeaderBoardImpl() {
+        this.results = new HashMap<>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void addResult(String playerName, MatchResult result) {
+        this.results.put(playerName, this.results.containsKey(playerName) ? this.evaluate(this.results.get(playerName), result) 
+                                                                          : this.evaluate(new Pair<>(0, 0), result));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Map<String, Pair<Integer, Integer>> getLeaderboard() {
+        return this.results.entrySet().stream()
+                .sorted((entry1, entry2) -> entry2.getValue().getX() - entry1.getValue().getX())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Optional<Pair<Integer, Integer>> getScoreFromPlayer(String playerName) {
+        return this.results.containsKey(playerName) ? Optional.of(this.results.get(playerName))
+                                                    : Optional.empty();
+    }
+
+    private Pair<Integer, Integer> evaluate(Pair<Integer, Integer> score, MatchResult result) {
+        switch (result) {
+            case VICTORY: return new Pair<>(score.getX() + 1, score.getY());
+            case DEFEAT: return new Pair<>(score.getX(), score.getY() + 1);
+            case DRAW: return score;
+            default: return score;
+        }
+    }
 }
