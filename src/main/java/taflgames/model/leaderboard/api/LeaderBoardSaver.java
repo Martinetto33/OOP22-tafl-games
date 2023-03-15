014f591d1c2a39c55f040becd9d9c77b92112d46
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2023-03-15 23:50:28 +0100

    chore + docs: corrected LeaderboardSaver interface and added javadoc

--- src/main/java/taflgames/model/leaderboard/api/LeaderBoardSaver.java
+++ src/main/java/taflgames/model/leaderboard/api/LeaderBoardSaver.java
@@ -3,10 +3,18 @@ package taflgames.model.leaderboard.api;
 /**
  * This interface models an object capable of saving Leaderboard types objects to file.
  */
-public interface LeaderBoardSaver {
-    
+public interface LeaderboardSaver {
+
+    /**
+     * Allows to save a given {@link taflgames.model.leaderboard.api.Leaderboard} to file.
+     * @param leaderboard the leaderboard to be saved
+     */
     void saveLeaderboard(Leaderboard leaderboard);
 
+    /**
+     * Reads and constructs a {@link taflgames.model.leaderboard.api.Leaderboard} from a file.
+     * @return
+     */
     Leaderboard retrieveFromSave();
 
 }
