00edeb4714187fa83a9da3004f131fbd36e1bf5d
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-03-16 14:30:35 +0100

    feat: add game over scene controller empty interface

---
+++ src/main/java/taflgames/view/scenecontrollers/GameOverController.java
@@ -0,0 +1,11 @@
+package taflgames.view.scenecontrollers;
+
+/**
+ * This interface extends a generic {@link SceneController}
+ * to add specific functions for the game over scene.
+ */
+public interface GameOverController extends SceneController {
+    /*
+     * TO DO
+     */
+}
