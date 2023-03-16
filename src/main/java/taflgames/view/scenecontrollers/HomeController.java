0e0d3fd2d5e31d3ff97447be8fab9e04b0575346
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-03-16 14:22:12 +0100

    feat: add home scene controller interface

---
+++ src/main/java/taflgames/view/scenecontrollers/HomeController.java
@@ -0,0 +1,14 @@
+package taflgames.view.scenecontrollers;
+
+/**
+ * This interface extends a generic {@link SceneController}
+ * to add specific functions for the home scene.
+ */
+public interface HomeController extends SceneController {
+
+    /**
+     * Closes the application.
+     */
+    void close();
+
+}
