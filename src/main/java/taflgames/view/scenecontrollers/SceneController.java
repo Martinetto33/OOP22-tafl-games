79d47a7e13f30bcca93df353d4a9a0f9ecbca152
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-03-16 14:23:10 +0100

    feat: add scene controller interface

---
+++ src/main/java/taflgames/view/scenecontrollers/SceneController.java
@@ -0,0 +1,18 @@
+package taflgames.view.scenecontrollers;
+
+/**
+ * This interface describes a controller for a generic scene.
+ */
+public interface SceneController {
+
+    /**
+     * Switches to the next scene.
+     */
+    void goToNextScene();
+
+    /**
+     * Switches to the previous scene.
+     */
+    void goToPreviousScene();
+
+}
