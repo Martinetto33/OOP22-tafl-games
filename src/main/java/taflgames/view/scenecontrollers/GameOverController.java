86b7aac27a4bbb8ef5f758446a8c02f8d386012d
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-03-18 16:32:04 +0100

    chore + docs: rename extended interface and update javadoc

--- src/main/java/taflgames/view/scenecontrollers/GameOverController.java
+++ src/main/java/taflgames/view/scenecontrollers/GameOverController.java
@@ -1,10 +1,10 @@
 package taflgames.view.scenecontrollers;
 
 /**
- * This interface extends a generic {@link SceneController}
- * to add specific functions for the game over scene.
+ * This interface extends a generic {@link BasicSceneController}
+ * to add specific functions for a {@link taflgames.view.scenes.GameOverScene}.
  */
-public interface GameOverController extends SceneController {
+public interface GameOverController extends BasicSceneController {
     /*
      * TO DO
      */
