3750e66e5249bfcc72a716165142098066e09b3f
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-03-18 12:59:20 +0100

    feat + style: change elements layout and size and remove magic strings and objects

--- src/main/java/taflgames/view/scenes/HomeScene.java
+++ src/main/java/taflgames/view/scenes/HomeScene.java
@@ -1,8 +1,11 @@
 package taflgames.view.scenes;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.Optional;
 
 import javax.swing.JPanel;
@@ -10,9 +13,7 @@ import javax.swing.JPanel;
 import taflgames.view.fontManager.FontManager;
 import taflgames.view.scenecontrollers.HomeController;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 
 /**
@@ -21,10 +22,14 @@ import javax.swing.JLabel;
 public class HomeScene extends AbstractScene {
 
     private static final String HOME = "Home";
+    private static final Color TRANSPARENT = new Color(0, 0, 0, 0);
+    private static final String BG_FILENAME = "home-background.jpeg";
     private static final String GAME_TITLE = "TAFL GAMES";
     private static final int TITLE_SIZE = 90;
     private static final String PLAY = "Play";
     private static final String EXIT = "Exit";
+    private static final double BTN_HEIGHT_PROP = 0.075;
+    private static final double BTN_WIDTH_PROP = 0.33;
 
     private final HomeController controller;
 
@@ -34,35 +39,47 @@ public class HomeScene extends AbstractScene {
      */
     public HomeScene(final HomeController controller) {
 
-        super(HOME, Optional.of("home-background.jpeg"));
+        super(HOME, Optional.of(BG_FILENAME));
 
         this.controller = controller;
 
         final JPanel scene = super.getScene();
 
+        final GridBagConstraints gbc = new GridBagConstraints();
         final FontManager fontManager = new FontManager();
 
-        final JPanel elementsPanel = new JPanel(new BorderLayout());
-        elementsPanel.setBackground(new Color(0, 0, 0, 0));
+        final JPanel elementsPanel = new JPanel(new GridBagLayout());
+        elementsPanel.setBackground(TRANSPARENT);
 
-        final JPanel titlePanel = new JPanel();
-        titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
+        final JPanel titlePanel = new JPanel(new GridBagLayout());
         final JLabel titleLabel = new JLabel(GAME_TITLE);
-        titleLabel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.NORTH;
         titleLabel.setFont(fontManager.getModifiedFont(TITLE_SIZE, Font.PLAIN));
         titleLabel.setForeground(Color.WHITE);
-        titlePanel.setBackground(new Color(0, 0, 0, 0));
+        titlePanel.setBackground(TRANSPARENT);
         titlePanel.add(titleLabel);
+        elementsPanel.add(titlePanel, gbc);
 
-        final JPanel buttonsPanel = new JPanel();
-        buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
+        final JPanel buttonsPanel = new JPanel(new GridBagLayout());
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10, 0, 10, 0);
         final JButton playButton = new JButton(PLAY);
-        playButton.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-        buttonsPanel.add(playButton);
+        playButton.setFont(AbstractScene.getDefaultFont());
+        playButton.setPreferredSize(new Dimension(
+            (int) (this.controller.getViewWidth() * BTN_WIDTH_PROP),
+            (int) (this.controller.getViewHeight() * BTN_HEIGHT_PROP)
+        ));
+        buttonsPanel.add(playButton, gbc);
         final JButton exitButton = new JButton(EXIT);
-        exitButton.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-        buttonsPanel.setBackground(new Color(0, 0, 0, 0));
-        buttonsPanel.add(exitButton);
+        exitButton.setFont(AbstractScene.getDefaultFont());
+        exitButton.setPreferredSize(new Dimension(
+            (int) (this.controller.getViewWidth() * BTN_WIDTH_PROP),
+            (int) (this.controller.getViewHeight() * BTN_HEIGHT_PROP)
+        ));
+        buttonsPanel.setBackground(TRANSPARENT);
+        buttonsPanel.add(exitButton, gbc);
 
         playButton.addActionListener((e) -> {
             this.controller.goToNextScene();
@@ -70,8 +87,7 @@ public class HomeScene extends AbstractScene {
 
         exitButton.addActionListener((e) -> this.controller.close());
 
-        elementsPanel.add(titlePanel, BorderLayout.NORTH);
-        elementsPanel.add(buttonsPanel, BorderLayout.CENTER);
+        elementsPanel.add(buttonsPanel, gbc);
 
         scene.add(elementsPanel);
     }
