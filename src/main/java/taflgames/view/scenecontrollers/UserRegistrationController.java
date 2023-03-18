5d1ae84e203a45446ae883fc740211ad902fb15a
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-03-18 16:31:48 +0100

    chore + docs: rename extended interface and update javadoc

--- src/main/java/taflgames/view/scenecontrollers/UserRegistrationController.java
+++ src/main/java/taflgames/view/scenecontrollers/UserRegistrationController.java
@@ -1,10 +1,10 @@
 package taflgames.view.scenecontrollers;
 
 /**
- * This interface extends a generic {@link SceneController}
- * to add specific functions for the user registration scene.
+ * This interface extends a generic {@link BasicSceneController}
+ * to add specific functions for a {@link taflgames.view.scenes.UserRegistrationScene}.
  */
-public interface UserRegistrationController extends SceneController {
+public interface UserRegistrationController extends BasicSceneController {
     /*
      * TO DO
      */
