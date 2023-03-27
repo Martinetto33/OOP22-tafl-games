9787c72f6ccc09643f01947755007ad472243a88
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-03-27 22:36:59 +0200

    test + refactor: add swapper move test and make source and destination selection tests more readable

--- src/test/java/taflgames/TestMatch.java
+++ src/test/java/taflgames/TestMatch.java
@@ -45,7 +45,7 @@ class TestMatch {
         final CellsCollectionBuilder cellsCollBuilder = new CellsCollectionBuilderImpl();
         final PiecesCollectionBuilder piecesCollBuilder = new PiecesCollectionBuilderImpl();
         try {
-            loader.loadClassicModeConfig(cellsCollBuilder, piecesCollBuilder);
+            loader.loadVariantModeConfig(cellsCollBuilder, piecesCollBuilder);
             final var pieces = piecesCollBuilder.build();
             final var cells = cellsCollBuilder.build();
             final int size = (int) Math.sqrt(cells.size());
@@ -87,18 +87,21 @@ class TestMatch {
          * Case: the current player does a valid selection.
          * For example, the attacker must be able to select piece initially at position (row=3, col=0).
          */
-        assertTrue(match.selectSource(new Position(3, 0)));
+        Position source = new Position(3, 0);
+        assertTrue(match.selectSource(source));
         /*
          * Case: the player does an invalid selection because the chosen piece that does not
          * belong to its team. For example, the attacker cannot chose the piece initially at position
          * (row=3, col=5).
          */
-        assertFalse(match.selectSource(new Position(3, 5)));
+        source = new Position(3, 5);
+        assertFalse(match.selectSource(source));
         /*
          * Case: the player does an invalid selection as the chosen cell does not contain a piece.
          * For example, the player cannot choose position (row=1, col=1) for the first move.
          */
-        assertFalse(match.selectSource(new Position(1, 1)));
+        source = new Position(1, 1);
+        assertFalse(match.selectSource(source));
 
         /*
          * Perform the same tests for the defender.
@@ -107,15 +110,18 @@ class TestMatch {
         /*
          * For example, the defender can choose the piece initially at position (row=5, col=3).
          */
-        assertTrue(match.selectSource(new Position(5, 3)));
+        source = new Position(5, 3);
+        assertTrue(match.selectSource(source));
         /*
          * For example, the defender cannot choose the piece initially at position (row=1, col=5)
          */
-        assertFalse(match.selectSource(new Position(1, 5)));
+        source = new Position(1, 5);
+        assertFalse(match.selectSource(source));
         /*
          * For example, the defender cannot choose the initially empty cell at position (row=1, col=1).
          */
-        assertFalse(match.selectSource(new Position(1, 1)));
+        source = new Position(1, 1);
+        assertFalse(match.selectSource(source));
     }
 
     /**
@@ -128,18 +134,24 @@ class TestMatch {
          * is empty (assuming that the selected piece is a classic piece).
          * For example, the piece at position (row=3, col=0) can move to position (row=2, col=0).
          */
-        assertTrue(match.selectDestination(new Position(3, 0), new Position(2, 0)));
+        Position source = new Position(3, 0);
+        Position dest = new Position(2, 0);
+        assertTrue(match.selectDestination(source, dest));
         /*
          * Case: the selected destination is invalid because the selected destination cell 
          * is not empty and the selected piece is not a swapper.
          * For example, basic piece at position (row=3, col=0) cannot move to position (row=3, col=5)
          */
-        assertFalse(match.selectDestination(new Position(3, 0), new Position(3, 5)));
+        source = new Position(3, 0);
+        dest = new Position(3, 5);
+        assertFalse(match.selectDestination(source, dest));
         /*
          * Case: the selected destination is invalid because the selected destination is an Exit.
          * For example, piece at position (row=3, col=0) cannot move to position (row=0, col=0).
          */
-        assertFalse(match.selectDestination(new Position(3, 0), new Position(0, 0)));
+        source = new Position(3, 0);
+        dest = new Position(0, 0);
+        assertFalse(match.selectDestination(source, dest));
     }
 
     /**
@@ -147,6 +159,9 @@ class TestMatch {
      */
     @Test
     void testMove() {
+        /*
+         * Case: move of a piece which is not a swapper.
+         */
         // The attacker moves the piece at (row=3, col=0) to position (row=3, col=4)
         Position source = new Position(3, 0);
         Position dest = new Position(3, 4);
@@ -155,6 +170,21 @@ class TestMatch {
         match.makeMove(source, dest);
         // Now the position (row=3, col=4) should be a valid source
         assertTrue(match.selectSource(dest));
+        /*
+         * Case: move of a swapper.
+         */
+        // Move piece at (9, 5) to be able to move the swapper at (10, 5)
+        source = new Position(9, 5);
+        dest = new Position(9, 4);
+        assertTrue(match.selectSource(source));
+        assertTrue(match.selectDestination(source, dest));
+        match.makeMove(source, dest);
+        // Now the swapper should be allowed to move to (7,5), even if a defender's piece is present.
+        source = new Position(10, 5);
+        dest = new Position(7, 5);
+        assertTrue(match.selectSource(source));
+        assertTrue(match.selectDestination(source, dest));
+        match.makeMove(source, dest);
     }
 
     /**
