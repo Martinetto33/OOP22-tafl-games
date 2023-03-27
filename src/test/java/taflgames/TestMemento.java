fbf3b8cb9c928b2f30519cffab75b1d6d3fd26c0
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2023-03-27 16:04:08 +0200

    test: added test for match initialisation

--- src/test/java/taflgames/TestMemento.java
+++ src/test/java/taflgames/TestMemento.java
@@ -1,11 +1,57 @@
 package taflgames;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import taflgames.controller.SettingsLoader;
+import taflgames.controller.SettingsLoaderImpl;
+import taflgames.model.Match;
+import taflgames.model.MatchImpl;
+import taflgames.model.board.code.BoardImpl;
+import taflgames.model.builders.CellsCollectionBuilder;
+import taflgames.model.builders.CellsCollectionBuilderImpl;
+import taflgames.model.builders.PiecesCollectionBuilder;
+import taflgames.model.builders.PiecesCollectionBuilderImpl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestMemento {
-        
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestMatch.class);
+
+    private Match match;
+
+    /**
+     * Initializes each test before its execution.
+     * {credits for this test to Elena Boschetti}
+     */
+    @BeforeEach
+    void init() {
+        final SettingsLoader loader = new SettingsLoaderImpl();
+        final CellsCollectionBuilder cellsCollBuilder = new CellsCollectionBuilderImpl();
+        final PiecesCollectionBuilder piecesCollBuilder = new PiecesCollectionBuilderImpl();
+        try {
+            /*
+             * Variant mode configuration is loaded, so that the match is tested also when
+             * using special pieces and cells.
+             */ 
+            loader.loadVariantModeConfig(cellsCollBuilder, piecesCollBuilder);
+            final var pieces = piecesCollBuilder.build();
+            final var cells = cellsCollBuilder.build();
+            final int size = (int) Math.sqrt(cells.size());
+            this.match = new MatchImpl(
+                new BoardImpl(pieces, cells, size)
+            );
+        } catch (final IOException ex) {
+            LOGGER.error("Cannot read configuration file. {}", ex.getMessage());
+            fail();
+        }
+    }
 }
