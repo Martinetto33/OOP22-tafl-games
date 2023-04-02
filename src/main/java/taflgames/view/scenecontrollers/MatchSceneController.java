83f61a754d7197c24dba2772409dcc13b97ee04d
Elena Boschetti <elena.boschetti@studio.unibo.it>
2023-04-02 18:27:29 +0200

    feat: add cells and pieces mapping getters

--- src/main/java/taflgames/view/scenecontrollers/MatchSceneController.java
+++ src/main/java/taflgames/view/scenecontrollers/MatchSceneController.java
@@ -1,9 +1,22 @@
 package taflgames.view.scenecontrollers;
-/**
- * Andre: non so se la devo fare io o no. SI PUO' TRANQUILLAMENTE ELIMINARE
- */
+
+import java.util.List;
+import java.util.Map;
+
+import taflgames.common.Player;
+import taflgames.common.code.Position;
+
 public interface MatchSceneController extends BasicSceneController {
+    
     /**
-     * to do
+     * @return the mapping of each position to the type of cell
+     * (and the cells components if present) at that position.
      */
+    Map<Position, List<String>> getCellsMapping();
+
+    /**
+     * @return the mapping of the positions 
+     */
+    Map<Player, Map<Position, String>> getPiecesMapping();
+
 }
